3f9a714f25e34d7b28ee7866466e4a5c71604cc7
Maximilian Sander <maximilian.sander1@tu-dresden.de>
2026-03-22 14:11:53 +0100

    Add correct PATH to cmake script

--- examples/turbChannel_wallModel_ML/udf.cmake
+++ examples/turbChannel_wallModel_ML/udf.cmake
@@ -1,5 +1,7 @@
 #set(CMAKE_CXX_FLAGS "-I/home/max/repos/distributed-data-queue/_build/install/include -L/home/max/repos/distributed-data-queue/_build/install/lib -lrdq")
 
-target_compile_options(udf PUBLIC "-I/home/max/repos/distributed-data-queue/_build/install/include")
-target_link_libraries(udf PUBLIC "/home/max/repos/distributed-data-queue/_build/install/lib/librdq.a")
+target_compile_options(udf PUBLIC "-I/home/max/repos/distributed-data-queue/_build_gcc15/install/include")
+target_link_libraries(udf PUBLIC "/home/max/repos/distributed-data-queue/_build_gcc15/install/lib/librdq.a")
+
+#target_compile_definitions(udf PUBLIC SMARTREDIS=1)
 
