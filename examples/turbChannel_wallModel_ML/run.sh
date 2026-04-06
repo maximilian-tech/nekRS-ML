4c9826cbe2c92b2b03d1a8541cdd073507a9e0d1
Maximilian Sander <maximilian.sander1@tu-dresden.de>
2026-04-06 14:53:33 +0200

    Fix Paths for Romeo

--- examples/turbChannel_wallModel_ML/run.sh
+++ examples/turbChannel_wallModel_ML/run.sh
@@ -1,15 +1,19 @@
 #!/bin/env bash
 set -x
-set -euo pipefail
 
 . $HOME/spack/share/spack/setup-env.sh
 spack env activate gcc15-tc
 
-PREFIX=/data/horse/ws/s0872522-testing/
+set -euo pipefail
+
+PREFIX=/data/horse/ws/s0872522-testing/nekRS-ML
 
 export NEKRS_HOME=$PREFIX/nekrs_install
-export PYTHONPATH=$PREFIX/remote-data-queue/_build/install/:.
+export PYTHONPATH=$NEKRS_HOME/3rd_party/rdq/:.
 
-mpirun -np 4 -- $NEKRS_HOME/bin/nekrs --setup turbChannel_train : \
-       -np 1 -- python3 ./torch_dataloader_consumer.py \
+
+LAUNCHER=mpiexec # srun not supported on ROMEO
+
+$LAUNCHER -n 4 -- $NEKRS_HOME/bin/nekrs --setup turbChannel_train : \
+       -n 1 -- python3 ./torch_dataloader_consumer.py \
        | tee log.out
--- examples/turbChannel_wallModel_ML/udf.cmake
+++ examples/turbChannel_wallModel_ML/udf.cmake
@@ -5,7 +5,7 @@ set(RDQ_TEST /home/max/repos/remote-data-queue/_build/install)
 #target_compile_options(udf PUBLIC "-I${RDQ_TEST}/include")
 #target_link_libraries(udf PUBLIC "${RDQ_TEST}/lib/librdq.a")
 target_compile_options(udf PUBLIC "-I${NEKRS_INSTALL_DIR}/3rd_party/rdq/include")
-target_link_libraries(udf PUBLIC "${NEKRS_INSTALL_DIR}/3rd_party/rdq/lib/librdq.a")
+target_link_libraries(udf PUBLIC "${NEKRS_INSTALL_DIR}/3rd_party/rdq/lib64/librdq.a")
 
 #target_compile_definitions(udf PUBLIC SMARTREDIS=1)
 
